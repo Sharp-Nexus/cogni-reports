071f89014a0e118392b1d278624aa5cf31b48528
Trevor Bernard <trevor.bernard@gmail.com>
2025-05-20 08:52:48 -0400

    feat(dev): add flake for hermetic development environment
    * Add flake support for python development
    
    * Ignore .direnv files

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.716632";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [self.overlays.default];
+          };
+        });
+  in {
+    overlays.default = final: prev: let
+      python = final.python312;
+      pythonPackages = python.pkgs;
+
+      developmentPackages = with pythonPackages; [
+        pip
+        virtualenv
+      ];
+
+      packages = with pythonPackages; [
+        boto3
+      ];
+    in {
+      pythonEnv = python.withPackages (ps: developmentPackages ++ packages);
+      pythonLambdaEnv = python.withPackages (ps: packages);
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          aws-sam-cli
+          awscli2
+          direnv
+          git
+          nix-direnv
+          pythonEnv
+        ];
+        shellHook = ''
+          echo "python --version"
+          echo "pip --version"
+        '';
+      };
+    });
+  };
+}
