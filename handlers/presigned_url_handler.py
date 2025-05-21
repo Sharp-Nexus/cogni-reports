b02c73c5d171a2567d1428401597e89b34417ea6
Trevor Bernard <trevor.bernard@gmail.com>
2025-05-21 18:11:26 -0400

    Revert "chore: remove explicitly defining a-amz-meta"
    This reverts commit 9d28a9ff460cc388d3888f2b0d53bc145b5f72f8.

--- handlers/presigned_url_handler.py
+++ handlers/presigned_url_handler.py
@@ -6,7 +6,7 @@ s3_client = boto3.client('s3')
 
 def handle_presigned_url_request(event, context):
     try:
-        key = event['queryStringParameters']['filename']
+        filename = event['queryStringParameters']['filename']
         bucket_name = os.environ['BUCKET_NAME']
         expiration = 600 # URL valid for 10 mins
 
@@ -14,7 +14,22 @@ def handle_presigned_url_request(event, context):
             ClientMethod='put_object',
             Params={
                 'Bucket': bucket_name,
-                'Key': key
+                'Key': filename,
+                'Metadata': {
+                    'x-amz-meta-simulationId': event['queryStringParameters']['simulationId'],
+                    'x-amz-meta-productId': event['queryStringParameters']['productId'],
+                    'x-amz-meta-materialId': event['queryStringParameters']['materialId'],
+                    'x-amz-meta-userId': event['queryStringParameters']['userId'],
+                    'x-amz-meta-teamId': event['queryStringParameters']['teamId'],
+                    'x-amz-meta-language': event['queryStringParameters']['language'],
+                    'x-amz-meta-character': event['queryStringParameters']['character'],
+                    'x-amz-meta-specialty': event['queryStringParameters']['specialty'],
+                    'x-amz-meta-adoptionContinuum': event['queryStringParameters']['adoptionContinuum'],
+                    'x-amz-meta-temperament': event['queryStringParameters']['temperament'],
+                    'x-amz-meta-situation': event['queryStringParameters']['situation'],
+                    'x-amz-meta-agent': event['queryStringParameters']['agent'],
+                    'x-amz-meta-disc': event['queryStringParameters']['disc']
+                }
             },
             ExpiresIn=expiration
         )
@@ -23,7 +38,7 @@ def handle_presigned_url_request(event, context):
             "statusCode": 200,
             "body": json.dumps({
                 "presignedUrl": presigned_url,
-                "accessUrl": "/callsim/" + key,
+                "accessUrl": "/callsim/" + filename,
             }),
             "headers": {
                 "Content-Type": "application/json",
